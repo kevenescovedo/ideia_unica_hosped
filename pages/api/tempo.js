9562d56799e899537225db97c66d4f44844ce5e5
kevenescovedo <kevenescovedo1916@gmail.com>
2021-01-22 17:11:41 -0300

    alteração api

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -4,6 +4,6 @@ function tempo(request,response) {
 let data = new Date();
 //a response serve para mandar algo pro usuario e a resposta em json
 
-response.json({date: data.toLocaleDateString("pt-Br"), texto: "essa é a nossa api"});
+response.json({date: data.toLocaleDateString("pt-Br", {day: "2-digit", month: "long"}), texto: "essa é a nossa api"});
 }
 export default tempo;
