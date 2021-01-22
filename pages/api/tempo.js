b05fb478d3004140e3d166be7fcc308c2022c6a2
kevenescovedo <kevenescovedo1916@gmail.com>
2021-01-22 17:08:50 -0300

    minha primeira api em nextjs

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -4,6 +4,6 @@ function tempo(request,response) {
 let data = new Date();
 //a response serve para mandar algo pro usuario e a resposta em json
 
-response.json({date: data.toLocaleDateString("pt_br"), texto: "essa é a nossa api"});
+response.json({date: data.toLocaleDateString("pt-Br"), texto: "essa é a nossa api"});
 }
 export default tempo;
