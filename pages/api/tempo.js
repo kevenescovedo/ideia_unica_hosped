a3a8a05944fa8c11cb3ba17c4982b28dc6563ffc
kevenescovedo <kevenescovedo1916@gmail.com>
2021-01-22 17:05:23 -0300

    nossa primeira api em next js

---
+++ pages/api/tempo.js
@@ -0,0 +1,9 @@
+//nossa api exemplo
+//um rota é um funcao js que recebe um request e response
+function tempo(request,response) {
+let data = new Date();
+//a response serve para mandar algo pro usuario e a resposta em json
+
+response.json({date: data.toLocaleDateString("pt_br"), texto: "essa é a nossa api"});
+}
+export default tempo;
