5d31d6eed86182d3f62884add4351487a6f4283c
kevenescovedo <kevenescovedo1916@gmail.com>
2021-01-24 17:18:23 -0300

    adição de uma rota que retorna informações sobre coronavirus globalmente

--- pages/api/global.js
+++ pages/api/global.js
@@ -1,7 +1,16 @@
 export default async function global(request, response) {
 let globalcases = await fetch("https://api.covid19api.com/summary");
 let json = await globalcases.json();
-response.json({novosCasosConfirmados: json.Global.NewConfirmed});
+response.json({
+    Api: " Api Infocovid feita por Keven Escovedo",
+    NovosCasosConfirmados: json.Global.NewConfirmed,
+
+    ToTalConfirmado:  json.Global.ToTalConfirmed,
+    NovasMortes:  json.Global.NewDeaths,
+    ToTalMortes:  json.Global.TotalDeaths,
+    NovasRecuperados:  json.Global.NewRecovered,
+    ToTalRecuperados:  json.Global.TotalRecovered,
+});
 
 
 }
